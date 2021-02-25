bfbfaa7a8986c4dce573141c4e0ce020a30d290d
Yuri <yuri.rampazo@outlook.com>
2021-02-25 13:49:37 -0300

    Lista 2 Revisada

--- Java/AULAS[TURMA-18]/src/LISTA2/Exer2.java
+++ Java/AULAS[TURMA-18]/src/LISTA2/Exer2.java
@@ -2,11 +2,12 @@ package LISTA2;
 
 import java.util.Scanner;
 
-public class Exercicio2 {
+public class Exer2 {
 		/* YURI MINA RAMPAZO - GENERATION BRASIL - TURMA 18
 		 *PROJETO:
 		 *2- Faça um programa que entre com 
 		 *três números e coloque em ordem crescente.*/
+	
 		public static void main(String[] args) {
 			Scanner sc = new Scanner (System.in);
 			System.out.print("Digite um número inteiro: ");
@@ -46,8 +47,35 @@ public class Exercicio2 {
 				System.out.print("\n2o.: " + numero1);
 				System.out.print("\n3o.: " + numero3);
 			}
-			else {
-				System.out.println("Os valores são iguais!");
+			else if (numero3 == numero1 && numero1 > numero2){
+				System.out.print("\n1o.: " + numero2);
+				System.out.print("\n2o.: " + numero1);
+				System.out.print("\n2o.: " + numero3);
+			}
+			else if (numero3 == numero2 && numero1 < numero2){
+				System.out.print("\n1o.: " + numero1);
+				System.out.print("\n2o.: " + numero2);
+				System.out.print("\n2o.: " + numero3);
+			}
+			else if (numero1 == numero2 && numero2 > numero3){
+				System.out.print("\n1o.: " + numero3);
+				System.out.print("\n2o.: " + numero2);
+				System.out.print("\n2o.: " + numero1);
+			}
+			else if (numero3 == numero1 && numero1 < numero2){
+				System.out.print("\n1o.: " + numero3);
+				System.out.print("\n1o.: " + numero1);
+				System.out.print("\n2o.: " + numero2);
+			}
+			else if (numero3 == numero2 && numero1 > numero2){
+				System.out.print("\n1o.: " + numero3);
+				System.out.print("\n1o.: " + numero2);
+				System.out.print("\n2o.: " + numero1);
+			}
+			else if (numero1 == numero2 && numero2 < numero3){
+				System.out.print("\n1o.: " + numero1);
+				System.out.print("\n1o.: " + numero2);
+				System.out.print("\n2o.: " + numero3);
 			}
 			sc.close();
 		}

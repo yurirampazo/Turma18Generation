bc59cfb5510bf3249d7299f3b45f5ffaae59abef
Yuri <yuri.rampazo@outlook.com>
2021-02-25 13:22:38 -0300

    Exercicio2 corrigido!

--- Java/AULAS[TURMA-18]/src/LISTA2/Exercicio2.java
+++ Java/AULAS[TURMA-18]/src/LISTA2/Exercicio2.java
@@ -3,12 +3,12 @@ package LISTA2;
 import java.util.Scanner;
 
 public class Exercicio2 {
-	class Exercicio1 {
+	static class Exercicio1 {
 		/* YURI MINA RAMPAZO - GENERATION BRASIL - TURMA 18
 		 *PROJETO:
 		 *2- Faça um programa que entre com 
 		 *três números e coloque em ordem crescente.*/
-		public void main(String[] args) {		// ctrl + shift + o = inicializa JAVA UTIL
+		public static void main(String[] args) {
 			Scanner sc = new Scanner (System.in);
 			System.out.print("Digite um número inteiro: ");
 			int numero1 = sc.nextInt();
@@ -17,35 +17,38 @@ public class Exercicio2 {
 			System.out.print("Digite mais um número inteiro: ");
 			int numero3 = sc.nextInt();
 			
-			if (numero1 > numero2 && numero2 >= numero3) {
-				System.out.println("1o.: " + numero3);
-				System.out.println("2o.: " + numero2);
-				System.out.println("3o.: " + numero1);
+			if (numero1 > numero2 && numero2 > numero3) {
+				System.out.print("\n1o.: " + numero3);
+				System.out.print("\n2o.: " + numero2);
+				System.out.print("\n3o.: " + numero1);
 			}	
-			else if (numero2 > numero1 && numero1 >= numero3) {
-				System.out.println("1o.: " + numero3);
-				System.out.println("2o.: " + numero1);
-				System.out.println("3o.: " + numero2);
+			else if (numero2 > numero1 && numero1 > numero3) {
+				System.out.print("\n1o.: " + numero3);
+				System.out.print("\n2o.: " + numero1);
+				System.out.print("\n3o.: " + numero2);
 			}
-			else if (numero2 > numero3 && numero3 >= numero1) {
-				System.out.println("1o.: " + numero1);
-				System.out.println("2o.: " + numero3);
-				System.out.println("3o.: " + numero2);	
+			else if (numero2 > numero3 && numero3 > numero1) {
+				System.out.print("\n1o.: " + numero1);
+				System.out.print("\n2o.: " + numero3);
+				System.out.print("\n3o.: " + numero2);	
 			}
-			else if (numero2 > numero1 && numero1 >= numero3) {
-				System.out.println("1o.: " + numero3);
-				System.out.println("2o.: " + numero1);
-				System.out.println("3o.: " + numero2);	
+			else if (numero2 > numero1 && numero1 > numero3) {
+				System.out.print("\n1o.: " + numero3);
+				System.out.print("\n2o.: " + numero1);
+				System.out.print("\n3o.: " + numero2);	
 			}
-			else if (numero3 > numero2 && numero2 >= numero1){
-				System.out.println("1o.: " + numero1);
-				System.out.println("2o.: " + numero2);
-				System.out.println("3o.: " + numero3);
+			else if (numero3 > numero2 && numero2 > numero1){
+				System.out.print("\n1o.: " + numero1);
+				System.out.print("\n2o.: " + numero2);
+				System.out.print("\n3o.: " + numero3);
 			}
-			else if (numero3 > numero1 && numero1 >= numero2){
-				System.out.println("1o.: " + numero2);
-				System.out.println("2o.: " + numero1);
-				System.out.println("3o.: " + numero3);
+			else if (numero3 > numero1 && numero1 > numero2){
+				System.out.print("\n1o.: " + numero2);
+				System.out.print("\n2o.: " + numero1);
+				System.out.print("\n3o.: " + numero3);
+			}
+			else {
+				System.out.println("Os valores são iguais!");
 			}
 			sc.close();
 		}

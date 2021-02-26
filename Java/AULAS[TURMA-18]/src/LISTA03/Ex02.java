579ef4664aa99602ba749759a89bbe6d14dac1c5
Yuri <yuri.rampazo@outlook.com>
2021-02-26 12:33:43 -0300

    Ex02

--- Java/AULAS[TURMA-18]/src/LISTA03/Ex02.java
+++ Java/AULAS[TURMA-18]/src/LISTA03/Ex02.java
@@ -1,10 +1,30 @@
 package LISTA03;
 
+import java.util.Scanner;
+
 public class Ex02 {
+	/* YURI MINA RAMPAZO - TURMA 18 - GENERATION BRASIL.
+	*PROPOSTA:
+	* 2- Ler 10 números e imprimir quantos são pares e quantos são ímpares. (FOR)*/
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+Scanner	leia = new Scanner(System.in);
+		
+		double numero = 0;
+		int numeroPar = 0, numeroImpar = 0;
+		
+		for (int i =0; i <=10; i++) {
+			System.out.println("Digite um número: ");
+			numero = leia.nextDouble();
+			if (numero % 2 == 0) {
+				numeroPar++;
+				
+			} else if (numero % 2 != 0) {
+				numeroImpar++;
+			}
+		}
+		System.out.println("Dos valores digitados " + numeroPar + " são pares.");	
+		System.out.println("Dos valores digitados " + numeroPar + " são ímpares.");
+	leia.close();
 	}
-
 }

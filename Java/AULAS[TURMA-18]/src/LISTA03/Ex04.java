1d5663c4f042d9af5fd5e6c20adb1577a4ce397c
Yuri <yuri.rampazo@outlook.com>
2021-02-26 22:01:21 -0300

    Lista 03 Encerrada

--- Java/AULAS[TURMA-18]/src/LISTA03/Ex04.java
+++ Java/AULAS[TURMA-18]/src/LISTA03/Ex04.java
@@ -21,63 +21,67 @@ public class Ex04 {
 
 
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		
-		int idade = 0, i = 0, outrosCalmos = 0, mulheresNervosas = 0, homens = 0; 
-		int maior40 = 0, menor18 = 0, calmas = 0, humor = 0;
-		char genero;
-		
-		while (i < 2) {
-			System.out.println("Digite sua idade: ");
-			idade = input.nextInt();
-			if (idade < 0) {
-				System.out.println("Você não pode ter idade negativa. Isso é biologicamente IMPOSSÍVEL!");
-				idade = input.nextInt();
-				input.nextLine();
-			}
-			System.out.println("Digite seu gênero: [F/O/M]");
-			genero = input.next().toUpperCase().charAt(0);
-			input.nextLine();
+		Scanner leia = new Scanner (System.in);
+			int idade;
+			char sexo; //(1-feminino / 2-masculino / 3-Outros)
+			char humor; //(1, se a pessoa era calma; 2, se a pessoa era nervosa e 3, se a pessoa era agressiva)
+			char opcao = 'S';
+			int contador = 1;
+			int pessoaCalma=0,mulherNervosa=0,homemAgressivo=0,outrosCalmo=0,pessoaNervosaMais40=0, pessoaCalmaMenos18=0;
 			
-			if (genero != 'F' && genero !='O' && genero != 'M') {
-				System.out.println("Você não digitou um gênero válido. Tente novamente [F/O/M]");
-				genero = input.next().toUpperCase().charAt(0);
-				input.nextLine();
-			}
-			System.out.println("Digite um número de acordo "
-					+ "com o seu húmor: \n1 - CALMO \n2 - NERVOSO \n3 - AGRESSIVO");
-			humor = input.next().charAt(0);
-			input.nextLine();
-			
-			if (humor != '1' && humor != '2' && humor != '3') {
-				System.out.println("Você não digitou um húmor conhecido. Tente novamente [1/2/3]");
-				humor = input.next().charAt(0);
-				input.nextLine();
+			while(contador <= 150)
+			{
+				//entradas
+				System.out.printf("Digite a idade da pessoa %d: ",contador);
+				idade = leia.nextInt();
 				
-			}else if (humor == '1' ) {
-				calmas++;
-				if (genero =='O' ) {
-					outrosCalmos++;
+				System.out.print("Digite o sexo da pessoa (1-feminino / 2-masculino / 3-Outros):");
+				sexo = leia.next().charAt(0);
+				
+				System.out.println("A pessoa é:");
+				System.out.println("1 - CALMA");
+				System.out.println("2 - NERVOSA");
+				System.out.print("3 - AGRESSIVA : ");
+				humor = leia.next().charAt(0);
+				
+				if (humor =='1') {
+					pessoaCalma++;
 				}
-			}if (humor == '2' && idade > 40) {
-				maior40++;
-			}if (humor == '1' && idade < 18) {
-				menor18++;
-			}if (humor == '2' && genero =='F') {
-				mulheresNervosas++;
-			}if (humor == '3' && genero == 'H') {
-				homens++;
+				if(humor == '2' && sexo == '1') {
+					mulherNervosa++;
+				}
+				if ( humor =='3'&& sexo =='2') {
+					homemAgressivo++;
+				}
+				if (humor == '1' && sexo =='3') {
+					outrosCalmo++;
+				}
+				if (humor =='2' && idade >= 40) {
+					pessoaNervosaMais40++;
+				}
+				if (humor == '1' && idade < 18){
+					pessoaCalmaMenos18++;
+				}
+				//opcão para sair do programa a qualquer momento
+				System.out.println("Continua S-sim ou N-não: ");
+				opcao = leia.next().toUpperCase().charAt(0);
+				if (opcao !='S') {
+					break;
+				}
+				//SOMA 1 AO CONTADOR E SE BATE 150 SAI DO LAÇO
+				contador++;
 			}
 			
+			System.out.println("Pessoa calma: "+pessoaCalma);
+			System.out.println("Mulher nerversosa :"+mulherNervosa);
+			System.out.println("Homem agressivo: "+homemAgressivo);
+			System.out.println("Outros calmo: "+outrosCalmo);
+			System.out.println("Pessoas nervosa acima 40 :"+pessoaNervosaMais40);
+			System.out.println("Pessoas calma abaix 18"+pessoaCalmaMenos18);
 			
-		 i++;
+			System.out.println("FIM DA PESQUISA!!!");
+			
+		leia.close();
 		}
-		System.out.println("Existem " + calmas + " pessoas calmas.");
-		System.out.println("Existem " + mulheresNervosas + " mulheres nervosas.");
-		System.out.println("Existem " + homens + " homens agressivos.");
-		System.out.println("Existem " + outrosCalmos + " outros calmos.");
-		System.out.println("Existem " + maior40 + " pessoas nervosas com mais de 40 anos.");
-		System.out.println("Existem " + menor18 + " pessoas calmas com menos de 18 anos.");
-	input.close();
 	}
 }

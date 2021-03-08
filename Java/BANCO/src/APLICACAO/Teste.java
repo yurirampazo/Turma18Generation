4effecb9eafd68160aa68517fc09b5454c5337f7
Yuri <yuri.rampazo@outlook.com>
2021-03-08 08:41:11 -0300

    iha

--- Java/BANCO/src/APLICACAO/Teste.java
+++ Java/BANCO/src/APLICACAO/Teste.java
@@ -5,6 +5,7 @@ package APLICACAO;
 import java.util.Scanner;
 
 import CLASSES.ContaCorrente;
+import CLASSES.ContaEmpresa;
 import CLASSES.ContaEspecial;
 import CLASSES.ContaPoupanca;
 
@@ -18,13 +19,14 @@ public class Teste {
 		double valor[] = new double [10];
 		final double limite = 1000; 
 		final double valorTalao = 1000;//Conta especial e ContaCorrente
+		final double emprestimoEmpresa = 10000;
 		char opcaoPagamento,opcaoContinuar = 'S', opcaoContaAtivaNaoAtiva, opcao;
 		boolean ativa = true;
 		 
 		ContaPoupanca contaPoupanca = new ContaPoupanca(numero,cpf,ativa, diaAniversarioPoupanca);
 		ContaCorrente contaCorrente = new ContaCorrente(numero,cpf,ativa,contadorTalao,valorTalao);
 		ContaEspecial contaEspecial = new ContaEspecial(numero,cpf,ativa,limite);
-		//ContaEmpresa contaEmpresa = new ContaEmpresa(numero,cpf,ativa,emprestimoEmpresa);
+		ContaEmpresa contaEmpresa = new ContaEmpresa(numero,cpf,ativa,emprestimoEmpresa);
 		//ContaEstudantil contaEstudantil = new ContaEstudantil(numero,cpf,ativa,limiteEstudantil);
 	
 		//ATT:  ASSIM QUE AS CLASSES ESTIVEREM PRONTAS UNIMOS ELAS AO PROJETO.
@@ -231,65 +233,69 @@ public class Teste {
 							
 					}
 
-			case 4 : {
+			case 4: {
 				
-				System.out.println("|============================================|");
-				System.out.println("|           [ BANCO GEN18 G-08 ]             |");
-				System.out.println("|   ---> Aqui seu dinheiro têm VALOR! <---   |");
-				System.out.println("|--------------------------------------------|");
-				System.out.println("|             { CONTA EMPRESA }              |");
-				System.out.println("|============================================|");
-				System.out.print("Digite o numero da sua conta: ");
-				numero = leia.nextInt();
+				//CASO SEJA CONTA ESPECIAL	
+				for (int i = 0; i < 15; i++) {
+					System.out.println();
+				}
 				
-				System.out.print("Insira o número do CPF:");  										
-		    	cpf = leia.next();
-				
-				
-				System.out.println("Digite 1 - Conta Ativa  ou  2 - Conta Inativa:");  												
-				opcaoContaAtivaNaoAtiva = leia.next().charAt(0);
-				if (opcaoContaAtivaNaoAtiva == '1') {
+					System.out.println("|============================================|");
+					System.out.println("|           [ BANCO GEN18 G-08 ]             |");
+					System.out.println("|   ---> Aqui seu dinheiro têm VALOR! <---   |");
+					System.out.println("|--------------------------------------------|");
+					System.out.println("|              { CONTA EMPRESA }             |");
+					System.out.println("|============================================|");
+					System.out.print("Digite o numero da sua conta: ");
+					numero = leia.nextInt();
 					
-					ativa = true;
-					System.out.println("Conta ativada.");
-					} 
-					else {
-					ativa = false;
-					System.out.println("Conta DESATIVADA!!!");
-					break;
-					}
-				
-			} break;
+					System.out.print("Insira o número do CPF:");  										
+			    	cpf = leia.next();
+					
+					
+					System.out.println("Digite 1 - Conta Ativa  ou  2 - Conta Inativa:");  												
+					opcaoContaAtivaNaoAtiva = leia.next().charAt(0);
+					if (opcaoContaAtivaNaoAtiva == '1') {
+						
+						ativa = true;
+						System.out.println("Conta ativada.");
+						} 
+						else {
+						ativa = false;
+						System.out.println("Conta DESATIVADA!!!");
+						break;
+						}
+					System.out.print("Saldo Atual: R$ " + contaEmpresa.getSaldo());
+					System.out.println();
+					//CALCULO DO SALDO
+					
+					for (int i = 0; i <10; i++) {	
+						System.out.print("Digite o valor da operação: R$ ");
+						valor[i] = leia.nextDouble();
+						
+						System.out.print("Débito ou crédito? [D/C]: ");
+						opcao = leia.next().toUpperCase().charAt(0);
+						
+						if (opcao == 'D') {
+							contaEmpresa.debito(valor);	
+							
+						}
+						else if (opcao == 'C') {
+							contaEmpresa.credito(valor);
+						
+						}
+						else {
+							System.out.println("Opção inválida!");
+							
+						}
+						
+						contaEmpresa.getEmprestimoEmpresa();		
+						System.out.println(i + 1+ "º Movimento. Deseja continuar? [S/N]: ");
+						opcaoContinuar = leia.next().toUpperCase().charAt(0);
+					
+					} 	}	
 			
-			case 5: {
-				
-				System.out.println("|============================================|");
-				System.out.println("|           [ BANCO GEN18 G-08 ]             |");
-				System.out.println("|   ---> Aqui seu dinheiro têm VALOR! <---   |");
-				System.out.println("|--------------------------------------------|");
-				System.out.println("|            { CONTA ESTUDANTIL }            |");
-				System.out.println("|============================================|");
-				System.out.print("Digite o numero da sua conta: ");
-				numero = leia.nextInt();
-				
-				System.out.print("Insira o número do CPF:");  										
-		    	cpf = leia.next();
-				
-				
-				System.out.println("Digite 1 - Conta Ativa  ou  2 - Conta Inativa:");  												
-				opcaoContaAtivaNaoAtiva = leia.next().charAt(0);
-				if (opcaoContaAtivaNaoAtiva == '1') {
-					
-					ativa = true;
-					System.out.println("Conta ativada.");
-					} 
-					else {
-					ativa = false;
-					System.out.println("Conta DESATIVADA!!!");
-					break;
-					}
-				
-			} break;
+			
 			
 			default: {
 
@@ -297,10 +303,10 @@ public class Teste {
 				break;
 			} 
 			
-			} 
+		} 
 			
-			System.out.println("Deseja continuar? [S/N]: ");
-			opcaoContinuar = leia.next().toUpperCase().charAt(0);
+		System.out.println("Deseja continuar? [S/N]: ");
+		opcaoContinuar = leia.next().toUpperCase().charAt(0);
 			
 		
 		
